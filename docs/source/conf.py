33ff8505bfa236eac990400f43ad130ab34097af
Grant Moore <chinchalinchin@gmail.com>
2023-07-29 08:43:24 -0400

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,22 +60,22 @@ html_theme = 'sphinx_material'
 html_theme_options = {
 
     # Set the name of the project to appear in the navigation.
-    'nav_title': 'Source Code',
+    'nav_title': 'Module: S3',
 
     # Set you GA account ID to enable tracking
     'google_analytics_account': 'UA-XXXXX',
 
     # Specify a base_url used to generate sitemap.xml. If not
     # specified, then no sitemap will be built.
-    'base_url': 'TODO',
+    'base_url': 'https://cumberland-cloud.github.io/modules-ecr/',
 
     # Set the color and the accent color
     'color_primary': 'blue',
     'color_accent': 'light-blue',
 
     # Set the repo location to get a badge with stats
-    'repo_url': 'TODO',
-    'repo_name': 'TODO',
+    'repo_url': 'https://github.com/cumberland-cloud/modules-s3',
+    'repo_name': 'modules-ecr',
 
     # Visible levels of the global TOC; -1 means unlimited
     'globaltoc_depth': 2,
--- variable.tf
+++ variable.tf
@@ -2,7 +2,7 @@ variable "bucket" {
     description     = <<EOT
     S3 Bucket configuration object. 
         
-    KMS key: If no KMS key is specified for the encryption of resources, one will be provisioned. If using a pre-existing bucket, the key output from the KMS module should be passed in under the `key` object.
+    KMS key: If no KMS key is specified for the encryption of resources, one will be provisioned. If using a pre-existing key, the key output from the KMS module should be passed in under the `key` object.
     
     Policy: Policy should be a JSON string. By default, a policy is generated that allows all users in the caller AWS account READ/WRITE access, with the exception of ACL operations, i.e. all ACL operations are explicitly denied. Any additional permissions passed in through the `policy` will be merged into the default policy through a `aws_iam_policy_document` data block.
     
