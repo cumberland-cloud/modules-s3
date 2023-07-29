f691f2393549e64b51c90c55ef2413e1343402ba
Grant Moore <chinchalinchin@gmail.com>
2023-07-29 09:09:55 -0400

    lock with version instead

--- module.tf
+++ module.tf
@@ -1,6 +1,8 @@
 module "kms" {
+    #checkov:skip=CKV_TF_1: "Ensure Terraform module sources use a commit hash"
+
     count           = local.conditions.provision_key ? 1 : 0
-    source          = "https://github.com/cumberland-cloud/modules-kms.git?ref=8842d57"
+    source          = "github.com/cumberland-cloud/modules-kms.git?ref=v1.0.0"
 
     key             = {
         alias       = "${local.name}-s3"
